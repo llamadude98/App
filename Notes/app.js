57160677e51330d32d40ce62cdc33882c3e7a861
llamadude98 <50802246+llamadude98@users.noreply.github.com>
2020-08-09 19:58:23 -0400

    Add files via upload

---
+++ Notes/app.js
@@ -0,0 +1,18 @@
+var express = require('express');
+
+var app = express();
+
+app.set('view engine', 'ejs');
+
+app.get('/',function(req,res){
+  res.sendFile(__dirname + '/index.html');
+});
+
+
+app.get('/profile/:name',function(req,res){
+  var data = {age:21, job:'egirl',hobbies:['running', 'swimming', 'dotaing']};
+  res.render('profile', {person: req.params.name,data:data});
+});
+
+
+app.listen(3000);
